861490bfd8c991f49ff27e5fdf1eb7555b7988bf
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-03-19 23:36:56 +0530

    Created a new component for app i.e Explore

--- Travigo-travel-bureau/src/components/Explore.jsx
+++ Travigo-travel-bureau/src/components/Explore.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Explore = ({title, placesAPI}) => {
   return (
-    <div>
-        <div></div>
-    </div>
+    <>
+        <div className='relative my-7 md:mt-3'>
+          <div className='travigo-container'>
+            <div className='flex items-center justify-center text-center mb-11 md:mb-7'>
+              <h1 className='text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900'>{title}</h1>
+            </div>
+            <div className='' >
+              {placesAPI?.map((val, i) => (
+                <div key={i} className=''>
+                    <div className='flex items-center justify-center'>
+                      <img src={val.placeImg} alt={val.location}  className='w-20 h-20 sm:w-16 sm:h-16 rounded-lg filter drop-shadow-lg' />
+                    </div>
+                    <div className=''>
+                      <h1>{val.location}</h1>
+                      <p>{val.distance}</p>
+
+                    </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
