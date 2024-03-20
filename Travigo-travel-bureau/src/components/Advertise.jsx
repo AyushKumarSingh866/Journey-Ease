ca9ac20fc24c020bc87d9a83370f91bfe0266e58
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-03-20 23:38:09 +0530

    Creation of advertisement components

---
+++ Travigo-travel-bureau/src/components/Advertise.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Advertise = ({brands}) => {
+
+  return (
+    <>
+    <div>
+        {brands?.map((val, i) =>(
+            <img src={val.iconSrc} alt="brands/icon"  className='w-44 h-auto object-fill xl:w-38 lg:w-32' />
+        ))}
+    </div>
+    </>
+  )
+}
+
+export default Advertise
