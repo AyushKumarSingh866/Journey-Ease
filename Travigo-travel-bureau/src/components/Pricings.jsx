7cfab0330fdff72f46a0292dbe383e0a29243513
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-03-31 20:37:43 +0530

    Types of plans for travigo app

--- Travigo-travel-bureau/src/components/Pricings.jsx
+++ Travigo-travel-bureau/src/components/Pricings.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import Pricecard from './Pricecard'
 
-const Pricings = ({ pricingapi }) => {
+const Pricings = ({ pricingapi: {title, text, btn1, btn2, plans} }) => {
   return (
     <>
-    <div>
-      <div>
-        <div>
-          <h1></h1>
-          <p></p>
-          <div>
-            <button type='button'>
+    <div className='my-16 relative'>
+      <div className='travigo-container'>
+        <div className='grid items-center justify-items-center mb-11 md:mb-7 text-center gap-2'>
+          <h1 className='text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900'>{title}</h1>
+          <p className='text-xl lg:text-sm text-gray-900'>{text}</p>
+          <div className='ring-1 ring-slate-100 rounded-full py-1.5 px-2 flex items-center justify-between gap-5 my-5 shadow-md shadow-slate-200'>
+            <button type='button' className='button-emrald sm:w-auto'>{btn1}
               </button>
-              <button type='button'>
+              <button type='button' className='button-light sm:w- shadow-slate-200 bg-slate-100'>{btn2}
 
               </button>
           </div>
         </div>
-        <div>
-          <Pricecard />
+        <div className=''>
+          {plans?.map((val, i) => (
+            <Pricecard key={i} val={plan}/>
+          ))}
         </div>
       </div>
     </div>
