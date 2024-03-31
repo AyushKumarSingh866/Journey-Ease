53c4fa7045e75dab1c92a30b079756390a8934e2
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-03-31 20:37:25 +0530

    Types of plans for travigo app

--- Travigo-travel-bureau/src/components/Pricecard.jsx
+++ Travigo-travel-bureau/src/components/Pricecard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Pricecard = () => {
+const Pricecard = ({plan: {planicon , title, text, plantype, plancontent}}) => {
+  // console.log(plan);
   return (
-    <div>Pricecard</div>
+    <>
+    <div className='p-5 rounded-lg bg-slate-50/5 ring-1 ring-slate-100 shadow-md shadow-slate-200'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-5'>
+          <div className='grid items-center'>
+            <img src={planicon} alt="planicon/img" className='' />
+          </div>
+          <div className='grid items-center'>
+            <h1 className=''>{title}</h1>
+            <p className=''>{text}</p>
+          </div>
+        </div>
+        <div className='grid items-center'><h1 className=''>{plantype}</h1></div>
+      </div>
+      {/* <div className=''></div> */}
+    </div>
+    </>
   )
 }
 
