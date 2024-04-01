a8543b7d51c344951f5d27cbe1fc9a2d29b20f46
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-04-01 23:15:02 +0530

    Final phase work initiated

--- Travigo-travel-bureau/src/App.jsx
+++ Travigo-travel-bureau/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Navbar, Hero, Footer, Memory, Explore, Advertise, } from './components';
-import { hero, navlinks, memory, placesAPI, brands, } from './data/travigoData';
+import { Navbar, Hero, Footer, Memory, Explore, Advertise,Pricings, Banner } from './components';
+import { hero, navlinks, memory, placesAPI, brands, pricingapi, bannerAPI} from './data/travigoData';
 const App = () => {
   return (
     <div>
@@ -10,7 +10,8 @@ const App = () => {
       <Memory memory={memory} />
       <Explore title="Explore The Beauty of World" placesAPI={placesAPI}/>
       <Advertise brands={brands}/>
-
+      <Pricings pricingapi={pricingapi}/>
+      <Banner bannerAPI={bannerAPI}/>
       <Footer/>
       
     </div>
