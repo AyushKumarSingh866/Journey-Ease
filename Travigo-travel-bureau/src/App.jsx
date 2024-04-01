5b6ceb5abeead61a86631eeae6ebb1304c3d5405
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2024-04-01 12:25:45 +0530

    .

--- Travigo-travel-bureau/src/App.jsx
+++ Travigo-travel-bureau/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Navbar, Hero, Footer, Memory, Explore, Advertise, Pricings } from './components';
-import { hero, navlinks, memory, placesAPI, brands, pricingapi} from './data/travigoData';
+import { Navbar, Hero, Footer, Memory, Explore, Advertise, } from './components';
+import { hero, navlinks, memory, placesAPI, brands, } from './data/travigoData';
 const App = () => {
   return (
     <div>
@@ -10,7 +10,7 @@ const App = () => {
       <Memory memory={memory} />
       <Explore title="Explore The Beauty of World" placesAPI={placesAPI}/>
       <Advertise brands={brands}/>
-      <Pricings  pricingapi = {pricingapi}/>
+
       <Footer/>
       
     </div>
